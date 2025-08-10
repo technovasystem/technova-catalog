76a6baebc080bf0f18690a7df588cb57e1cd9603
technovasystem <soporte.technovasystem@gmail.com>
2025-08-10 05:16:59 -0600

    Add files via upload

---
+++ catalogo-universal-tecnova/img/productos_todos.js
@@ -0,0 +1,481 @@
+// productos.js
+function obtenerProductos() {
+  return [
+    { 
+      nombre: "Manzanas Rojas", 
+      descripción: 
+      precio: 150 CUP, 
+      imagen: "img/Manzanas_Rojas.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Pulóver Negro",  
+      descripción: 
+      precio: 1,600 CUP, 
+      imagen: "img/Pulóver_Negro.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Televisor 32'' Samsung",  
+      descripción: 
+      precio: 40,000 CUP,  
+      imagen: "img/Televisor_32_Samsung.jpg", 
+      estado: "Agotado", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Auriculares Bluetooth",  
+      descripción: 
+      precio: 5,000 CUP, 
+      imagen: "img/Auriculares_Bluetooth.jpg", 
+      estado: "Disponible", 3
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Zapatos Deportivos",  
+      descripción: 
+      precio: 7,000 CUP,    
+      imagen: "img/Zapatos_Deportivos.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Laptop HP 14''",  
+      descripción: 
+      precio: 150,000 CUP,  
+      imagen: "img/Laptop_HP_14.jpg", 
+      estado: "Agotado", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Reloj Inteligente",  
+      descripción: 
+      precio: 7,000 CUP, 
+      imagen: "img/Reloj_Inteligente.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Cafetera Express",  
+      descripción: 
+      precio: 51,000 CUP, 
+      imagen: "img/Cafetera_Express.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Cámara Fotográfica",  
+      descripción: 
+      precio: 71,000 CUP, 
+      imagen: "img/Cámara_Fotográfica.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Bicicleta de Montaña",  
+      descripción: 
+      precio: 237,000 CUP, 
+      imagen: "img/Bicicleta_de_Montaña.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" },
+
+    { 
+      nombre: "Tablet Samsung",  
+      descripción: 
+      precio: 32,000 CUP, 
+      imagen: "img/Tablet_Samsung.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Bocinas Bluetooth",  
+      descripción: 
+      precio: 32,000 CUP, 
+      imagen: "img/Bocinas_Bluetooth.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Mochila Tecnológica",  
+      descripción: 
+      precio: 9,000 CUP, 
+      imagen: "img/Mochila_Tecnológica.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Ventilador de Mesa",  
+      descripción: 
+      precio: 24,000 CUP, 
+      imagen: "img/Ventilador_de_Mesa.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Licuadora Oster",  
+      descripción: 
+      precio: 20,000 CUP, 
+      imagen: "img/Licuadora_Oster.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Plancha de Ropa",  
+      descripción: 
+      precio: 18,000 CUP, 
+      imagen: "img/Plancha_de_Ropa.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Silla Gamer",  
+      descripción: 
+      precio: 51,000 CUP, 
+      imagen: "img/Silla_Gamer.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Microondas LG",  
+      descripción: 
+      precio: 67,000 CUP, 
+      imagen: "img/Microondas_LG.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Nevera Milexus",  
+      descripción: 
+      precio: 316,000 CUP, 
+      imagen: "img/Nevera_Milexus.jpg", 
+      estado: "Agotado", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Combo de Reina y Arrocera",  
+      descripción: 
+      precio: 43,000 CUP, 
+      imagen: "img/Combo_de_Reina_y_Arrocera.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Teclado Mecánico",  
+      descripción: 
+      precio: 19,000 CUP, 
+      imagen: "img/Teclado_Mecánico.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Lavadora Océano 5Kg",  
+      descripción: 
+      precio: 46,000 CUP, 
+      imagen: "img/Lavadora_Océano.jpg", 
+      estado: "Disponible",
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Consola PlayStation 5",  
+      descripción: 
+      precio: 191,000 CUP, 
+      imagen: "img/Consola_PlayStation_5.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+     },
+     { 
+      nombre: "Teléfono Sansung F15 5G",  
+      descripción: 
+      precio: 25,000 CUP, 
+      imagen: "img/Teléfono_Sansung_F15_5G.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+     },
+     { 
+      nombre: "Monitor Computadora",  
+      descripción: 
+      precio: 51,000 CUP, 
+      imagen: "img/Monitor_Computadora.jpg", 
+      estado: "Agotado", 
+      whatsapp: "50123456" 
+     },
+     { 
+      nombre: "Sobrecama Camera",  
+      descripción: 
+      precio: 5,000 CUP, 
+      imagen: "img/Sobrecama_Camera.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+     },
+     { 
+      nombre: "Licras Largas",  
+      descripción: 
+      precio: 1,600 CUP, 
+      imagen: "img/Licras_Largas.jpg", 
+      estado: "Agotado", 
+      whatsapp: "50123456" 
+     },
+     { 
+      nombre: "Piscina de Patio",  
+      descripción: 
+      precio: 95,000 CUP, 
+      imagen: "img/Piscina_de_Patio.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+     },
+     { 
+      nombre: "Pc de Escritorio",  
+      descripción: 
+      precio: 375,000 CUP, todo incluido
+      imagen: "img/Pc_de_Escritorio.jpg",
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+     },
+     { 
+      nombre: "Juego de Baño",  
+      descripción: 
+      precio: 63,000 CUP, 
+      imagen: "img/Juego_de_Baño.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+     },
+     { 
+      nombre: "Juego de Cuarto",  
+      descripción: 
+      precio: 120,000 CUP,  
+      imagen: "img/Juego_de_Cuarto.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+     },
+     { 
+      nombre: "Toallas Grandes",  
+      descripción: 
+      precio: 1,500 CUP, 
+      imagen: "img/Toallas-Grandes.jpg", 
+      estado: "Agotado", 
+      whatsapp: "50123456" 
+     },
+     { 
+      nombre: "Columpio Familiar",  
+      descripción: 
+      precio: 158,000 CUP, 
+      imagen: "img/Columpio_Familiar.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+     },
+     { 
+      nombre: "Set de Peluquería",  
+      descripción: 
+      precio: 11,000 CUP, 
+      imagen: "img/Set_de_Peluquería.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Impresora Epson",  
+      descripción: 
+      precio: 134,000 CUP, 
+      imagen: "img/Impresora_Epson.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Cocina Inflaroja",  
+      descripción: 
+      precio: 22,000 CUP, 
+      imagen: "img/Cocina_Inflaroja.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Juego de Ollas",  
+      descripción: 
+      precio: 22,000 CUP, 
+      imagen: "img/Juego_de_Ollas.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Cama Matrimonial y Colchón",  
+      descripción: 
+      precio: 15,000 CUP, 
+      imagen: "img/Cama_Matrimonial_y_Colchón.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Litera con Colchón",  
+      descripción: 
+      precio: 70,000 CUP, 
+      imagen: "img/Litera_con_Colchón.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Juego de Sábanas",  
+      descripción: 
+      precio: 1,800 CUP, 
+      imagen: "img/Juego_de_Sábanas.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Carrito Eléctrico para Niños",  
+      descripción: 
+      precio: 111,000 CUP, 
+      imagen: "img/Carrito_Eléctrico_para_Niños.jpg", 
+      estado: "Disponible", whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Lonchera Electrica",  
+      descripción: 
+      precio: 7,900 CUP, 
+      imagen: "img/Lonchera_Electrica.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Negocio de Manicura y Pedicura con Maquinas y Productos Nuevos",  
+      descripción: 
+      precio: 178,000 CUP, 
+      imagen: "img/Manicura_Pedicura.jpg", 
+      estado: "Disponible", whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Perfume Mariposa",  
+      descripción: 
+      precio: 5,000 CUP, 
+      imagen: "img/Perfume_Mariposa.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Freidora Eléctrica",  
+      descripción: 
+      precio: 28,000 CUP, 
+      imagen: "img/Freidora_Eléctrica.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Negocio Completo de Impresiones, Sublimación y Personalización",  
+      descripción: 
+      precio: 1,383,000 CUP‬, 
+      imagen: "img/Negocio_de_Impresiones.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Sombrilla de Playa",  
+      descripción: 
+      precio: 6,000 CUP, 
+      imagen: "img/Sombrilla_de_Playa.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Casa de Campaña",  
+      descripción: 
+      precio: 20,000 CUP, 
+      imagen: "img/Casa_de_Campaña.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Juego de Herramientas",  
+      descripción: 
+      precio: 18,000 CUP, 
+      imagen: "img/Juego_de_Herramientas.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Máquina de coser",  
+      descripción: 
+      precio: 39,500 CUP, 
+      imagen: "img/Máquina_de_coser.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Planta Decorativa",  
+      descripción: 
+      precio: 2,000 CUP, 
+      imagen: "img/Planta_Decorativa.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Juego de Vajilla",  
+      descripción: 
+      precio: 8,500 CUP, 
+      imagen: "img/Juego_de_Vajilla.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Platero Inoxidable",  
+      descripción: 
+      precio: 25,000 CUP, 
+      imagen: "img/Platero_Inoxidable.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Lámpara de Mesa",  
+      descripción: 
+      precio: 9,000 CUP, 
+      imagen: "img/Lámpara_de_Mesa.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Juego de Sala Clásico",  
+      descripción: 
+      precio: 70,000 CUP, 
+      imagen: "img/Juego_de_Sala_Clásico.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Cocina Híbrida",  
+      descripción: 
+      precio: 178,000 CUP‬, 
+      imagen: "img/Cocina_Híbrida.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Sofá Cama de Tres Plazas",  
+      descripción: 
+      precio: 230,000 CUP, 
+      imagen: "img/Sofá_Cama_de_Tres_Plazas.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Motor de Agua",  
+      descripción: 
+      precio: 30,000 CUP, 
+      imagen: "img/Motor_de_Agua.jpg", 
+      estado: "Disponible", whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Generador Eléctrico",  
+      descripción: 
+      precio: 474,000 CUP, 
+      imagen: "img/Generador_Eléctrico.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" 
+    },
+    { 
+      nombre: "Spiit 1.5 Toneladas",  
+      descripción: 
+      precio: 194,000 CUP,
+      imagen: "img/Spiit.jpg", 
+      estado: "Disponible", 
+      whatsapp: "50123456" }
+  ];
+}
